1bcca63b22afffe5cc31c52facaa21bf7c1d514b
Easyman121 <2854470@gmail.com>
2023-03-11 13:25:22 +0300

    Легендарный рыцарь 2000

---
+++ Knight/Knight/Item.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knight
+{
+    class Item
+    {
+        private int _weight = 0;
+        private int _price = 0;
+
+        private string _name = "Неизвестный предмет";
+        private string _description = "Только богам известно, на что он способен";
+
+        public Item(string name, string description)
+        {   
+            _name = name;
+            _description = description;
+        }
+
+        public Item(string name, string description, int weight, int price)
+        {
+            _name = name;
+            _description = description;
+            _weight = weight;
+            _price = price;
+        }
+
+        public Item()
+        {
+
+        }
+
+        public string ToString()
+        {
+            string info = "";
+            info += "\n   Предмет:   " + _name;
+            info += "\n   Описание:   " + _description;
+
+            info += "\n   Вес:   " + _weight.ToString() + " кг.";
+            info += "\n   Цена:   " + _price.ToString() + " тугр.";
+
+
+            return info;
+        }
+
+    }
+}
---
+++ Knight/Knight/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Knight
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+
+
+        public void DoSmth()
+        {
+            Console.Clear();
+
+            Item item = new Item();
+            WriteLine(item.ToString());
+        }
+
+        public void WriteLine(string txt)
+        {
+            Console.Text += txt;
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            DoSmth();
+        }
+    }
+}
